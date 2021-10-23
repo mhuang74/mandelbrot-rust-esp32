36d814b674d7df365f52045fcfffd0cd17619b1f
Michael S. Huang <mhuang74@gmail.com>
2021-10-23 18:29:21 +0800

    cleaned up skeleton version of rust-esp32-std-hello from ivmarkov

---
+++ src/main.rs
@@ -0,0 +1,192 @@
+#![allow(clippy::single_component_path_imports)]
+//#![feature(backtrace)]
+
+use std::sync::{Condvar, Mutex};
+use std::{cell::RefCell, env, sync::Arc, thread, time::*};
+
+use anyhow::*;
+use log::*;
+
+
+use embedded_svc::httpd::registry::*;
+use embedded_svc::httpd::*;
+use embedded_svc::ipv4;
+use embedded_svc::ping::Ping;
+use embedded_svc::wifi::*;
+
+use esp_idf_svc::httpd as idf;
+use esp_idf_svc::netif::*;
+use esp_idf_svc::nvs::*;
+use esp_idf_svc::ping;
+use esp_idf_svc::sysloop::*;
+use esp_idf_svc::wifi::*;
+
+use esp_idf_hal::prelude::*;
+
+
+
+#[allow(dead_code)]
+const SSID: &str = env!("RUST_ESP32_STD_HELLO_WIFI_SSID");
+#[allow(dead_code)]
+const PASS: &str = env!("RUST_ESP32_STD_HELLO_WIFI_PASS");
+
+
+thread_local! {
+    static TLS: RefCell<u32> = RefCell::new(13);
+}
+
+fn main() -> Result<()> {
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    // Get backtraces from anyhow; only works for Xtensa arch currently
+    #[cfg(arch = "xtensa")]
+    env::set_var("RUST_BACKTRACE", "1");
+
+    #[allow(unused)]
+    let peripherals = Peripherals::take().unwrap();
+    #[allow(unused)]
+    let pins = peripherals.pins;
+
+    #[allow(unused)]
+    let netif_stack = Arc::new(EspNetifStack::new()?);
+    #[allow(unused)]
+    let sys_loop_stack = Arc::new(EspSysLoopStack::new()?);
+    #[allow(unused)]
+    let default_nvs = Arc::new(EspDefaultNvs::new()?);
+
+    let mutex = Arc::new((Mutex::new(None), Condvar::new()));
+
+    let httpd = httpd(mutex.clone())?;
+
+    let mut wait = mutex.0.lock().unwrap();
+
+    #[allow(unused)]
+    let cycles = loop {
+        if let Some(cycles) = *wait {
+            break cycles;
+        } else {
+            wait = mutex.1.wait(wait).unwrap();
+        }
+    };
+
+    for s in 0..3 {
+        info!("Shutting down in {} secs", 3 - s);
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    drop(httpd);
+    info!("Httpd stopped");
+
+    {
+        drop(wifi);
+        info!("Wifi stopped");
+    }
+
+
+
+    Ok(())
+}
+
+#[allow(unused_variables)]
+fn httpd(mutex: Arc<(Mutex<Option<u32>>, Condvar)>) -> Result<idf::Server> {
+    let server = idf::ServerRegistry::new()
+        .at("/")
+        .get(|_| Ok("Hello, world!".into()))?
+        .at("/foo")
+        .get(|_| bail!("Boo, something happened!"))?
+        .at("/bar")
+        .get(|_| {
+            Response::new(403)
+                .status_message("No permissions")
+                .body("You have no permissions to access this page".into())
+                .into()
+        })?;
+
+
+    server.start(&Default::default())
+}
+
+
+
+#[allow(dead_code)]
+fn wifi(
+    netif_stack: Arc<EspNetifStack>,
+    sys_loop_stack: Arc<EspSysLoopStack>,
+    default_nvs: Arc<EspDefaultNvs>,
+) -> Result<Box<EspWifi>> {
+    let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, default_nvs)?);
+
+    info!("Wifi created, about to scan");
+
+    let ap_infos = wifi.scan()?;
+
+    let ours = ap_infos.into_iter().find(|a| a.ssid == SSID);
+
+    let channel = if let Some(ours) = ours {
+        info!(
+            "Found configured access point {} on channel {}",
+            SSID, ours.channel
+        );
+        Some(ours.channel)
+    } else {
+        info!(
+            "Configured access point {} not found during scanning, will go with unknown channel",
+            SSID
+        );
+        None
+    };
+
+    wifi.set_configuration(&Configuration::Mixed(
+        ClientConfiguration {
+            ssid: SSID.into(),
+            password: PASS.into(),
+            channel,
+            ..Default::default()
+        },
+        AccessPointConfiguration {
+            ssid: "aptest".into(),
+            channel: channel.unwrap_or(1),
+            ..Default::default()
+        },
+    ))?;
+
+    info!("Wifi configuration set, about to get status");
+
+    let status = wifi.get_status();
+
+    if let Status(
+        ClientStatus::Started(ClientConnectionStatus::Connected(ClientIpStatus::Done(ip_settings))),
+        ApStatus::Started(ApIpStatus::Done),
+    ) = status
+    {
+        info!("Wifi connected");
+
+        ping(&ip_settings)?;
+    } else {
+        bail!("Unexpected Wifi status: {:?}", status);
+    }
+
+    Ok(wifi)
+}
+
+
+fn ping(ip_settings: &ipv4::ClientSettings) -> Result<()> {
+    info!("About to do some pings for {:?}", ip_settings);
+
+    let ping_summary =
+        ping::EspPing::default().ping(ip_settings.subnet.gateway, &Default::default())?;
+    if ping_summary.transmitted != ping_summary.received {
+        bail!(
+            "Pinging gateway {} resulted in timeouts",
+            ip_settings.subnet.gateway
+        );
+    }
+
+    info!("Pinging done");
+
+    Ok(())
+}
+
+
+
