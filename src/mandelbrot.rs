7bc99228eb954cd4de93951efcee732ed49b25c7
Michael S. Huang <mhuang74@gmail.com>
2021-10-23 21:31:37 +0800

    add in remaining calculation functions

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -23,3 +23,45 @@ fn test_pixel_to_point() {
                             Complex { re: -0.5, im: -0.75});
 
 }
+
+/// Try to determine if complex point `c` is in the Mandelbrot set, using at most `limit` iterations to decide.
+fn escape_time(c: Complex<f32>, limit: usize) -> Option<usize> {
+
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+
+        z = z * z + c;
+    }
+
+    None
+}
+
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+/// 
+/// The `bounds` argument gives the width and height of the buffer `pixels`
+/// which holds one grayschale pixel per byte. The `upper_left` and `lower_left`
+/// arguments specify points on the complex plane corresponding to the upper-left 
+/// and lower-right corners of the pixel buffer.
+fn render(pixels: &mut [u8], 
+    bounds: (usize, usize), 
+    upper_left: Complex<f32>, 
+    lower_right: Complex<f32>) {
+
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+    for column in 0..bounds.0 {
+        let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+        pixels[row*bounds.0 + column] = match escape_time(point, 255) {
+            None => 0,
+            Some(count) => 255 - count as u8
+        };
+    }
+    }
+
+}
+
